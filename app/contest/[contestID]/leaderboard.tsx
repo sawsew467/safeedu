2c7ec1e951ca8cfd88d3512cc05c3e522dce796b
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2024-11-01 15:54:37 +0700

    sua lai header common

---
+++ app/contest/[contestID]/leaderboard.tsx
@@ -0,0 +1,10 @@
+import LeaderBoardModule from '@/components/features/LeaderBoardModule'
+import React from 'react'
+
+const LeaderBoard = () => {
+    return (
+        <LeaderBoardModule />
+    )
+}
+
+export default LeaderBoard
