daef71f13627b54e8aab155df47e107c068511d1
sawsew467 <thangtvb.dev@gmail.com>
2024-11-03 23:30:38 +0700

    feat: edit game

--- healper/data/game.ts
+++ healper/data/game.ts
@@ -6,7 +6,7 @@ import game_image_3 from "@/assets/images/game_images/game_image_3.png";
 export const GAME_DATA: GameDataType[] = [
   {
     id: "game1",
-    title: "Cóc vượt chông gai",
+    title: "Cóc chinh phục",
     image: game_image_1,
   },
   {
@@ -14,24 +14,24 @@ export const GAME_DATA: GameDataType[] = [
     title: "Cóc vượt chông gai",
     image: game_image_2,
   },
-  {
-    id: "game3",
-    title: "Cóc vượt chông gai",
-    image: game_image_3,
-  },
-  {
-    id: "game4",
-    title: "Cóc vượt chông gai",
-    image: game_image_1,
-  },
-  {
-    id: "game5",
-    title: "Cóc vượt chông gai",
-    image: game_image_3,
-  },
-  {
-    id: "game6",
-    title: "Cóc vượt chông gai",
-    image: game_image_2,
-  },
+  // {
+  //   id: "game3",
+  //   title: "Cóc vượt chông gai",
+  //   image: game_image_3,
+  // },
+  // {
+  //   id: "game4",
+  //   title: "Cóc vượt chông gai",
+  //   image: game_image_1,
+  // },
+  // {
+  //   id: "game5",
+  //   title: "Cóc vượt chông gai",
+  //   image: game_image_3,
+  // },
+  // {
+  //   id: "game6",
+  //   title: "Cóc vượt chông gai",
+  //   image: game_image_2,
+  // },
 ];
